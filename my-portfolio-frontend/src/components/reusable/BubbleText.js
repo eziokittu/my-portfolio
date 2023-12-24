402755ea87499b79402bb99726eee1ef108fcbbf
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2023-12-25 02:01:30 +0530

    Hero section almost done

---
+++ my-portfolio-frontend/src/components/reusable/BubbleText.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+
+const BubbleText = ({text}) => {
+  return (
+    <Bubble myText={text}/>
+  );
+};
+
+const Bubble = ({myText}) => {
+  useEffect(() => {
+    const spans = document.querySelectorAll(".hover-text span");
+
+    spans.forEach((span) => {
+      span.addEventListener("mouseenter", function () {
+        this.style.fontWeight = "900";
+        this.style.color = "rgb(238, 242, 255)";
+
+        const leftNeighbor = this.previousElementSibling;
+        const rightNeighbor = this.nextElementSibling;
+
+        if (leftNeighbor) {
+          leftNeighbor.style.fontWeight = "500";
+          leftNeighbor.style.color = "rgb(199, 210, 254)";
+        }
+        if (rightNeighbor) {
+          rightNeighbor.style.fontWeight = "500";
+          rightNeighbor.style.color = "rgb(199, 210, 254)";
+        }
+      });
+
+      span.addEventListener("mouseleave", function () {
+        this.style.fontWeight = "100";
+        this.style.color = "rgb(165, 180, 252)";
+
+        const leftNeighbor = this.previousElementSibling;
+        const rightNeighbor = this.nextElementSibling;
+
+        if (leftNeighbor) {
+          leftNeighbor.style.fontWeight = "100";
+          leftNeighbor.style.color = "rgb(165, 180, 252)";
+        }
+
+        if (rightNeighbor) {
+          rightNeighbor.style.fontWeight = "100";
+          rightNeighbor.style.color = "rgb(165, 180, 252)";
+        }
+      });
+    });
+  }, []);
+
+  return (
+    <h2 className="hover-text text-7xl md:text-6xl lg:text-8xl font-thin text-indigo-300">
+      <Text>{myText}</Text>
+    </h2>
+  );
+};
+
+const Text = ({ children }) => {
+  return (
+    <span>
+      {children.split("").map((child, idx) => (
+        <span
+          style={{
+            transition: "0.35s font-weight, 0.35s color",
+          }}
+          key={idx}
+        >
+          {child}
+        </span>
+      ))}
+    </span>
+  );
+};
+
+export default BubbleText;
--- my-portfolio-frontend/src/components/reusable/Hero.js
+++ my-portfolio-frontend/src/components/reusable/Hero.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 // importing reusable components
 import TypingComponent from './TypingComponent';
+import ShuffleHero from './ShuffleHero';
+import BubbleText from "./BubbleText";
 
 // importing data from local files
 import heroTextAnimateData from "../../Data/profile/heroTextAnimateData.json";
@@ -10,19 +12,50 @@ import profileData from "../../Data/profile/profileData.json";
 function Hero() {
 
   return (
-    <div>
+    <div className="w-full pt-12 px-2 xsm:px-12 md:flex items-center justify-around mx-auto">
+      
+      {/* The welcome text */}
+      <div className='pr-4 lg:pr-16 xl:pr-44 2xl:pr-80'>
 
-      {/* Intro Text */}
-      <div>
-        <p>Hey there,</p>
-        <p>I am</p>
-        <p>{profileData.name.firstName}</p>
-        <p>{profileData.name.lastName}</p>
+        <div className="mb-4 text-3xl  text-indigo-500 ">
+          Hey There,
+        </div>
+
+        <div className="">
+          <span className='text-blue-300 text-xl '>I am </span>
+          <span 
+            className="text-7xl md:text-6xl lg:text-8xl 
+            font-semibold text-blue-600"
+          ><BubbleText text={profileData.name.firstName}/></span>
+        </div>
+        
+        <h3 className="text-4xl md:text-6xl 
+        font-semibold text-blue-500"
+        ><BubbleText text={profileData.name.lastName}/></h3>
+
+        {/* Typing Text Animation  */}
+        <div className="text-base md:text-lg text-slate-700 my-4 md:my-6">
+          <TypingComponent texts={heroTextAnimateData.data} speedTyping={60} speedDelete={30}/>
+        </div>
+
+        {/* About Me Button */}
+        <button 
+          className="w-[160px] flex justify-center
+          bg-indigo-500 text-white font-normal hover:font-bold  rounded 
+          transition hover:bg-indigo-600 active:scale-95 py-2 px-4
+          duration-500 "
+        >
+          <p>About Me</p>
+          <svg className='w-6 h-6 ml-2'
+            xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+            <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm5.247 8l-5.247 6.44-5.263-6.44-.737.678 6 7.322 6-7.335-.753-.665z"/>
+          </svg>
+        </button>
       </div>
 
-      {/* Typing Text Animation  */}
-      <div className="">
-        <TypingComponent texts={heroTextAnimateData.data} speedTyping={60} speedDelete={30}/>
+      {/* The grid for shuffling hero images */}
+      <div className='w-full p-2 xsm:p-8 bg-black/20 rounded-lg '>
+        <ShuffleHero />
       </div>
       
     </div>
