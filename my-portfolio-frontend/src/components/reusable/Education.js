0699bdfe809bb29aa7daebfd4443accbac6b3966
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2023-12-26 18:39:12 +0530

    education section complete

--- my-portfolio-frontend/src/components/reusable/Education.js
+++ my-portfolio-frontend/src/components/reusable/Education.js
@@ -12,29 +12,31 @@ const Education = () => {
 {/* drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] */}
       {/* All the contents of section */}
       <div 
-        className='mx-2 xsm:mx-12 md:mx-auto md:w-[700px] lg:w-[900px] p-2 xsm:p-4
-        bg-amber-500/40 hover:bg-amber-300/70 rounded-lg group
+        className='mx-2 xsm:mx-auto
+        w-fit xsm:w-5/6 sm:w-4/5 lg:w-3/4 xl:w-3/5 2xl:w-1/2
+        p-2 xsm:p-4
+        bg-yellow-500/10 hover:bg-yellow-500/20 rounded-lg group/edu1
         shadow-[0_0_5px_black] hover:shadow-[0_0_15px_black] transition duration-500'
       >
 
         {/* Section Title */}
         <div className='text-center text-2xl 2xsm:text-3xl sm:text-4xl font-bold border-b-[4px] border-amber-950
-          w-fit mx-auto bg-clip-text text-transparent
-          bg-gradient-to-r from-orange-400 via-yellow-300 to-amber-500
-          group-hover:from-amber-800 group-hover:via-orange-500 group-hover:to-orange-800
+          w-fit mx-auto bg-clip-text text-transparent transition duration-300
+          bg-gradient-to-r from-amber-600 via-yellow-300 to-amber-600
+          group-hover/edu1:from-yellow-300 group-hover/edu1:via-amber-400 group-hover/edu1:to-yellow-300
           flex gap-2 items-center'
         >
 
           {/* Education Icon */}
           <div 
             className=' text-black 
-            group-hover:animate-[bounce_1s_ease-in-out_infinite_.3s]'
+            group-hover/edu1:animate-[bounce_1s_ease-in-out_infinite_.3s]'
           >
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
               viewBox="0 0 16 16" 
               fill="currentColor" 
-              className="w-10 h-10 group-hover:animate-pulse">
+              className="w-10 h-10 group-hover/edu1:animate-pulse">
               <path d="M7.25 3.688a8.035 8.035 0 0 0-4.872-.523A.48.48 0 0 0 2 3.64v7.994c0 .345.342.588.679.512a6.02 6.02 0 0 1 4.571.81V3.688ZM8.75 12.956a6.02 6.02 0 0 1 4.571-.81c.337.075.679-.167.679-.512V3.64a.48.48 0 0 0-.378-.475 8.034 8.034 0 0 0-4.872.523v9.268Z" />
             </svg>
           </div>
@@ -45,7 +47,7 @@ const Education = () => {
         </div>
 
         {/* Section Body */}
-        <div>
+        <div className='flex flex-col gap-2 xsm:gap-4'>
           <EducationCard props={educationData.data[0]}/>
           <EducationCard props={educationData.data[1]}/>
           <EducationCard props={educationData.data[2]}/>
--- my-portfolio-frontend/src/components/reusable/EducationCard.js
+++ my-portfolio-frontend/src/components/reusable/EducationCard.js
@@ -3,51 +3,73 @@ import React from 'react'
 const EducationCard = ({props}) => {
   return (
     <div 
-      className='group bg-yellow-500/50 hover:bg-yellow-500
-      rounded-lg p-2 sm:flex'
+      className='group/edu2 bg-yellow-50 hover:bg-yellow-100
+      rounded-lg p-2 sm:flex '
     >
 
       {/* Institute Image */}
       <div 
-        className='h-[200px] w-fit'
+        className='h-[250px] lg:h-[300px] xl:h-[350px]
+        w-fit sm:w-5/12 lg:w-2/5 xl:w-1/3
+        mb-4 mx-auto sm:mr-4 sm:mb-0'
       >
         <img
-          className='h-full w-full rounded-t-lg sm:rounded-tr-none sm:rounded-l-lg'
+          className='h-full w-full rounded-t-2xl sm:rounded-tr-none sm:rounded-l-2xl'
           src={props.image}
           alt={props.alt}
         />
       </div>
 
       {/* Education Details */}
-      <div>
+      <div className='flex flex-col
+        w-fit sm:w-7/12 lg:w-3/5 xl:w-2/3
+        sm:h-[250px] lg:h-[300px] xl:h-[350px]
+        '>
 
         {/* Degree Achieved */}
-        <div>
+        <div 
+          className='font-bold text-2xl lg:text-3xl xl:text-4xl
+          text-center text-gray-600 group-hover/edu2:text-gray-900'
+        >
           <p>{props.achieved}</p>
         </div>
 
+        {/* Horizontal line */}
+        <hr className='border-yellow-900 my-1'/>
+
         {/* Name and Board */}
-        <div>
+        <div 
+          className='text-lg lg:text-xl xl:text-2xl
+          text-gray-600 group-hover/edu2:text-gray-900'>
 
           {/* Institute Name */}
-          <div>
-            <p>{props.name}</p>
+          <div className=''>
+          <span className='font-bold mr-2'>Institute:</span>
+            <span className=''>{props.name}</span>
           </div>
 
           {/* Institute Board */}
-          <div>
-            <p>{props.board}</p>
+          <div className=''>
+            <span className='font-bold mr-2'>Board:</span>
+            <span className=''>{props.board}</span>
           </div>
 
         </div>
 
+        {/* Horizontal line */}
+        <hr className='sm:mt-auto border-yellow-900 my-1'/>
+
         {/* Institute Study Year Duration */}
-        <div>
+        <div 
+          className='font-bold 
+          text-green-700 group-hover/edu2:text-green-900
+          text-xl lg:text-2xl xl:text-3xl flex'
+        >
           <span>{props.yearStart}</span>
           <span>-</span>
           <span>{props.yearEnd}</span>
-          <span>|</span>
-          <span>{props.status}</span>
+          <span className='mx-auto'>~</span>
+          <span className=''>{props.status}</span>
         </div>
 
       </div>
