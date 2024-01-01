adcbfb79fba15b19206889e9847f309f20e0413e
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-01-01 19:02:20 +0530

    fixed a warning 1

--- my-portfolio-frontend/src/App.js
+++ my-portfolio-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 // importing reusable components
 // import DisableRightClick from './components/ReusablePages/DisableRightClick';
 // import DisableInspectMenu from './components/ReusablePages/DisableInspectMenu';
